ef62f2abfc5de749edde4d4a43387bcb934c4b9b
zschopper <zschopper@gmail.com>
2024-03-06 12:45:56 +0100

    axiosService kezeli a tokent (ideiglenes megoldás), alapbeállítás a témához (világos)

--- src/models/axiosService.js
+++ src/models/axiosService.js
@@ -10,6 +10,42 @@ const axiosInstance = axios.create({
     }
 });
 
+// ha van tokenünk, elküldjük
+axiosInstance.interceptors.request.use(request => {
+    const token = localStorage.getItem('accessToken');
+    if (token) {
+        request.headers.Authorization = `Bearer ${token}`;
+    }
+    return request;
+});
+
+// Response interceptor to handle token expiration
+axiosInstance.interceptors.response.use(
+    response => response,
+    async error => {
+        const originalRequest = error.config;
+        if (error.response.status === 401 && !originalRequest._retry) {
+            originalRequest._retry = true;
+            const refreshToken = localStorage.getItem('refreshToken');
+            try {
+                // Attempt to refresh the token
+                const response = await axios.post(`${config.serverUrl}/refresh`, { refreshToken });
+                if (response.status === 200) {
+                    // Store new token and update the original request
+                    localStorage.setItem('accessToken', response.data.accessToken);
+                    axios.defaults.headers.common['Authorization'] = `Bearer ${response.data.accessToken}`;
+                    return axiosInstance(originalRequest);
+                }
+            } catch (e) {
+                console.log('Refresh token invalid', e);
+                // Handle the case where the refresh token is also invalid (e.g., logout the user)
+            }
+        }
+        // Return any other errors not related to token expiration
+        return Promise.reject(error);
+    }
+);
+
 const axiosService = {
     get: (url, config = {}) => axiosInstance.get(url, config),
     post: (url, data, config = {}) => axiosInstance.post(url, data, config),
--- src/models/config.js
+++ src/models/config.js
@@ -5,6 +5,7 @@
 const config = {
     serverUrl: 'http://localhost:8000/api/staff/', // a záró / jel kell a végére
     sidebarOpened: true,
+    theme: 1, // 0 sötét, 1 világos
 };
 
 export default config;
