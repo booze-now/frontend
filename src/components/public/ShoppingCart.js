6073ad0b95f2d8692d3e91b60789d2497cd4b849
Szisz06 <118608773+Szisz06@users.noreply.github.com>
2024-04-08 22:28:44 +0200

    Kosárkezelés fejlesztése

--- src/components/public/ShoppingCart.js
+++ src/components/public/ShoppingCart.js
@@ -1,10 +1,11 @@
 import { Table, Button } from "react-bootstrap";
 import { useCart } from "contexts/CartContext";
 import CounterInput from "react-bootstrap-counter";
-import './drinks.css'
+import "./drinks.css";
+import { Link } from "react-router-dom";
 
 export default function ShoppingCart() {
-  const { detailedCartItems, removeFromCart, addToCart } = useCart();
+  const { detailedCartItems, removeFromCart, addToCart, calculateCartTotal } = useCart();
   return (
     <div>
       <h1>Shopping Cart</h1>
@@ -24,24 +25,23 @@ export default function ShoppingCart() {
           {detailedCartItems().map((item, index) => (
             <tr key={index}>
               <td>{index + 1}</td>
-              <td>{item.name}</td>
-              <td>{item.unitPrice} </td>
+              <td>#{item.id} {item.name}</td>
+              <td>{item.unitPrice} Ft</td>
               <td>
                 {item.amount} {item.unit}
               </td>
               <td>
                 <div className="counter-input-wrapper">
                   <CounterInput
-                    min={1}
                     max={99}
                     value={item.quantity}
                     onChange={(value) =>
-                      addToCart(item.id, item.amount, item.unit, value)
+                      addToCart(item.id, item.amount, item.unit, value, "set")
                     }
                   />
                 </div>
               </td>
-              <td>i cant count</td>
+              <td>{item.total}</td>
               <td>
                 <Button
                   variant="danger"
@@ -57,11 +57,18 @@ export default function ShoppingCart() {
         </tbody>
       </Table>
       <p>
-        Total Price:{" "}
-        {detailedCartItems().reduce(
-          (total, item) => total + item.unit_price * item.quantity,
-          0
-        )}
+        Total Price: {calculateCartTotal()}
+      </p>
+      <Button
+        variant="secondary"
+        //onClick={() => order()}
+      >
+        Order for ___ Ft !
+      </Button>
+      <p>
+        {" "}
+        <Link to="/terms-of-use">{`Felhasználási feltételek`}</Link>et
+        elolvastam
       </p>
     </div>
   );
--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -2,6 +2,7 @@ import { createContext, useContext, useState } from "react";
 import { useConfig } from "./ConfigContext";
 import { useApi } from "./ApiContext";
 import { useMessages } from "./MessagesContext.js";
+import { useTranslation } from "./TranslationContext";
 
 const CartContext = createContext();
 
@@ -14,6 +15,7 @@ export const CartProvider = ({ children }) => {
   const { get } = useApi();
   const { realm } = useConfig();
   const { addMessage } = useMessages();
+  const { __ } = useTranslation();
 
   const [cartItems, setCartItems] = useState(() => {
     // LOAD
@@ -23,16 +25,15 @@ export const CartProvider = ({ children }) => {
     return items;
   });
 
-
   const loadDrinks = async () => {
     if (!loaded && realm) {
       try {
-        const response = await get("menu-tree")
+        const response = await get("menu-tree");
         const drinks = response.data;
         const drinkList = updateDrinkList(drinks);
         setMenu(drinks);
         setDrinkList(drinkList);
-        setLoaded(true)
+        setLoaded(true);
       } catch (error) {
         // error.response.status == 401
         addMessage("danger", error.statusText);
@@ -43,19 +44,29 @@ export const CartProvider = ({ children }) => {
 
   const updateDrinkList = (drinks) => {
     const drinkList = {};
-    Object.keys(drinks ?? {}).forEach((key) => { // főkategóriák
+    Object.keys(drinks ?? {}).forEach((key) => {
+      // főkategóriák
       const outCat = drinks[key];
-      Object.keys(outCat.drinks ?? {}).forEach((key) => { // főkategóris italok
-        const drink = outCat.drinks[key]
+      Object.keys(outCat.drinks ?? {}).forEach((key) => {
+        // főkategóris italok
+        const drink = outCat.drinks[key];
+        if (drink.unit === null) {
+          drink.unit = __("glass");
+        }
         drinkList[drink.id] = drink;
       });
-      Object.keys(outCat.subcategory ?? {}).forEach((key2) => { // alkategóriák
-        const inCat = outCat.subcategory[key2]
-        Object.keys(inCat.drinks ?? {}).forEach((key) => { // főkategóris italok
-          const drink = inCat.drinks[key]
+      Object.keys(outCat.subcategory ?? {}).forEach((key2) => {
+        // alkategóriák
+        const inCat = outCat.subcategory[key2];
+        Object.keys(inCat.drinks ?? {}).forEach((key) => {
+          // főkategóris italok
+          const drink = inCat.drinks[key];
+          if (drink.unit === null) {
+            drink.unit = __("glass");
+          }
           drinkList[drink.id] = drink;
         });
-      })
+      });
     });
     return drinkList;
   };
@@ -70,30 +81,40 @@ export const CartProvider = ({ children }) => {
     return drinkList;
   };
 
-
   const removeFromCart = (drink_id, amount, unit) => {
+    amount = Number(amount);
     const key = `${drink_id}|${amount}|${unit}`;
     const newCartItems = { ...cartItems };
     delete newCartItems[key];
     setCartItems(newCartItems);
+    console.log("removeFromCart:", key, "removed");
     localStorage.setItem(CART_KEY, JSON.stringify(newCartItems));
   };
 
-  const addToCart = (drink_id, amount, unit, quantityToAdd) => {
-    const key = `${drink_id}|${amount}|${unit}`;
+  const addToCart = (drink_id, amount, unit, quantityToAdd, mode = "add") => {
+    amount = Number(amount);
+    console.log("addToCart", { drink_id, amount, unit, quantityToAdd, mode });
+    const key = `${drink_id}|${amount}|${unit ?? ""}`;
     const currentQuantity = cartItems[key] || 0;
-    const newQuantity = currentQuantity + quantityToAdd;
-  
+    let newQuantity = quantityToAdd;
+    if (mode === "add") {
+      newQuantity += currentQuantity;
+    }
+
+    if (newQuantity < 0) {
+      newQuantity = 0;
+    }
+
     const cartItemsCopy = { ...cartItems };
     cartItemsCopy[key] = newQuantity;
-  
+
     if (cartItemsCopy[key] <= 0) {
       delete cartItemsCopy[key];
     }
-  
+
     setCartItems(cartItemsCopy);
     localStorage.setItem(CART_KEY, JSON.stringify(cartItemsCopy));
-  
+
     const drink = drinkList[drink_id];
     if (drink) {
       const parsedAmount = parseFloat(amount);
@@ -101,47 +122,81 @@ export const CartProvider = ({ children }) => {
         (u) => parseFloat(u.amount) === parsedAmount && u.unit === unit
       );
       if (selectedUnit) {
-        const unitPrice = selectedUnit.unit_price;
-        addMessage("success", `Added ${drink.name} to cart`);
+        if (mode === "add" && quantityToAdd > 0) {
+          addMessage("success", "Added :drink to cart", { drink: drink.name });
+        }
+        if (mode === "add" && quantityToAdd < 0) {
+          addMessage("success", "Removed :drink from cart", {
+            drink: drink.name,
+          });
+        }
       } else {
-        addMessage("warning", `Selected unit not found for drink_id: ${drink_id}, amount: ${amount}, unit: ${unit}`);
+        addMessage(
+          "warning",
+          `Selected unit not found for drink_id: ${drink_id}, amount: ${amount}, unit: ${unit}`
+        );
       }
     } else {
       addMessage("warning", `Drink not found for drink_id: ${drink_id}`);
     }
   };
-  
+
   const detailedCartItems = () => {
     const drinkList = getDrinkList();
-  
-    return Object.entries(cartItems).map(([key, quantity]) => {
-      const [drink_id, amount, keyUnit] = key.split("|");
-      const unit = keyUnit === 'null' ? null : keyUnit;
-      const drink = drinkList[drink_id];
-      if (drink) {
-        const parsedAmount = parseFloat(amount);
-        const selectedUnit = drink.units.find(
-          (u) => parseFloat(u.amount) === parsedAmount && u.unit === unit
-        );
-        if (selectedUnit) {
-          const unitPrice = selectedUnit.unit_price;
-          return {
-            id: Number(drink_id),
-            name: drink.name || `Drink #${drink_id}`,
-            amount,
-            unit: unit || 'glass',
-            unitPrice,
-            quantity,
-          };
+
+    const ret = Object.entries(cartItems)
+      .map(([key, quantity]) => {
+        const { drink_id, amount, unit } = parseKey(key);
+        const drink = drinkList[drink_id];
+        if (drink) {
+          const selectedUnit = drink.units.find(
+            (u) => parseFloat(u.amount) === amount && u.unit === unit
+          );
+          if (selectedUnit) {
+            const unitPrice = Number(selectedUnit.unit_price);
+            return {
+              id: drink_id,
+              name: drink.name || `Drink #${drink_id}`,
+              amount,
+              unit: unit,
+              unitPrice,
+              quantity: Number(quantity),
+              key,
+              total: quantity * unitPrice,
+            };
+          } else {
+            return null;
+          }
         } else {
-          return null; 
+          return null;
         }
-      } else {
-        return null;
-      }
-    }).filter((item) => item !== null); 
+      })
+      .filter((item) => item !== null);
+    console.log("details", ret);
+    return ret;
   };
-  
+
+  const calculateCartTotal = () => {
+    let grandTotal = 0;
+    Object.entries(cartItems).forEach(([key, value]) => {
+      const { drink_id, amount, unit } = parseKey(key);
+      const drink = drinkList[drink_id];
+      let unitPrice = 0;
+      const selectedUnit = drink.units.find(
+        (u) => parseFloat(u.amount) === amount && u.unit === unit
+      );
+      if (selectedUnit) {
+        unitPrice = selectedUnit.unit_price;
+      } else {
+        unitPrice = NaN; // ez itt hiba
+      }
+
+      // console.log("cartItem", key, value, "*", drink, "total:", grandTotal);
+      grandTotal += Number(value) * Number(unitPrice);
+    });
+
+    return grandTotal;
+  };
 
   return (
     <CartContext.Provider
@@ -149,6 +204,7 @@ export const CartProvider = ({ children }) => {
         getMenu,
         cartItems,
         detailedCartItems,
+        calculateCartTotal,
         addToCart,
         removeFromCart,
         getDrinkList,
@@ -157,6 +213,14 @@ export const CartProvider = ({ children }) => {
       {children}
     </CartContext.Provider>
   );
+};
+
+const parseKey = (key) => {
+  const [keyId, keyAmount, keyUnit] = key.split("|");
+  const drink_id = Number(keyId);
+  const amount = Number(keyAmount);
+  const unit = keyUnit === "" ? null : keyUnit;
+  return { drink_id, amount, unit };
 };
 
 export const useCart = () => useContext(CartContext);
